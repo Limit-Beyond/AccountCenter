3fe424c75e2190b5082bcab286880bc74a8dc4da
OrElse <493969376@qq.com>
2020-01-12 21:45:11 +0800

    here

---
+++ AccountCenter/account-api/src/test/java/com/petmall/accountserviceapi/AccountserviceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmall.accountserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountserviceapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
